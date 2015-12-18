edda88d90f3c1be080090dc53e65b677b3ef9a22
Patrick Herrmann <patrickwherrmann@gmail.com>
2015-12-18 02:47:22 -0800

    Solve day 8 part 1

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,1 +1,20 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Day8 where
+
+totalCharsDiff :: String -> Int
+totalCharsDiff s = sum $ map charDiff $ lines s
+
+charDiff :: String -> Int
+charDiff s = length s + 2 - chars s
+
+chars :: String -> Int
+chars ('\\':'\\':cs) = 1 + chars cs
+chars ('\\':'"':cs) = 1 + chars cs
+chars ('\\':'x':(isHex -> True):(isHex -> True):cs) = 1 + chars cs
+chars (_:cs) = 1 + chars cs
+chars [] = 0
+
+isHex :: Char -> Bool
+isHex = (`elem` hexChars)
+  where hexChars = ['0'..'9'] ++ ['a'..'f'] ++ ['A'..'F']
