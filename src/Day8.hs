c7a6e81b2a5ac86458d363821dcd4c46eefeba9c
Patrick Herrmann <patrickwherrmann@gmail.com>
2015-12-18 02:54:55 -0800

    Finish day 8

--- src/Day8.hs
+++ src/Day8.hs
@@ -3,7 +3,13 @@
 module Day8 where
 
 totalCharsDiff :: String -> Int
-totalCharsDiff s = sum $ map charDiff $ lines s
+totalCharsDiff = sum . map charDiff . lines
+
+totalCharsDiff2 :: String -> Int
+totalCharsDiff2 = sum . map charDiff2 . lines
+
+charDiff2 :: String -> Int
+charDiff2 s = specialChars s + 2
 
 charDiff :: String -> Int
 charDiff s = length s + 2 - chars s
@@ -14,6 +20,10 @@ chars ('\\':'"':cs) = 1 + chars cs
 chars ('\\':'x':(isHex -> True):(isHex -> True):cs) = 1 + chars cs
 chars (_:cs) = 1 + chars cs
 chars [] = 0
+
+specialChars :: String -> Int
+specialChars = length . filter isSpecial
+  where isSpecial c = c == '\\' || c == '"'
 
 isHex :: Char -> Bool
 isHex = (`elem` hexChars)
