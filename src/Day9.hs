083a0b13c8d5d40173aa549f127023d6dc2f321b
Patrick Herrmann <patrickwherrmann@gmail.com>
2015-12-18 19:30:15 -0800

    Finish day 9

--- src/Day9.hs
+++ src/Day9.hs
@@ -11,25 +11,28 @@ import qualified Data.Map as Map
 type City = String
 type Distance = Int
 type Path = [City]
+type MeasuredPath = (Path, Distance)
 type Edge = ((City, City), Distance)
+type EdgeMap = Map.Map (City, City) Distance
 
-data Graph = Graph [City] (Map.Map (City, City) Distance)
+data Graph = Graph [City] EdgeMap
 
-solution :: String -> (Path, Distance)
-solution = shortestPath . constructGraph . parseEdges
+parseGraph :: String -> Graph
+parseGraph = constructGraph . parseEdges
 
-shortestPath :: Graph -> (Path, Distance)
-shortestPath g = minimumBy (comparing snd) ps'
-  where
-    ps = paths g
-    ps' = map (\p -> (p, pathDistance g p)) ps
+longestPath :: Graph -> MeasuredPath
+longestPath = maximumBy (comparing snd) . measuredPaths
 
-pathDistance :: Graph -> Path -> Int
-pathDistance (Graph _ es) p = sum $ map (es Map.!) pairs
+shortestPath :: Graph -> MeasuredPath
+shortestPath = minimumBy (comparing snd) . measuredPaths
+
+measuredPaths :: Graph -> [MeasuredPath]
+measuredPaths (Graph cs es) = map (\p -> (p, pathDistance es p)) ps
+  where ps = permutations cs
+
+pathDistance :: EdgeMap -> Path -> Int
+pathDistance es p = sum $ map (es Map.!) pairs
   where pairs = zip p (tail p)
-
-paths :: Graph -> [Path]
-paths (Graph cs _) = permutations cs
 
 constructGraph :: [Edge] -> Graph
 constructGraph es = Graph cities edges
