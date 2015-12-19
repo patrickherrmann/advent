ec444c0de6acbe784d991ab5c10e9e6f745ab8e7
Patrick Herrmann <patrickwherrmann@gmail.com>
2015-12-18 23:51:14 -0800

    Finish day 12

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
+
 module Day12 where
 
 import Data.Aeson
@@ -8,10 +11,28 @@ import Data.ByteString.Lazy.Char8 (pack)
 import qualified Data.HashMap.Strict as HM
 
 findAndAddNumbers :: String -> Int
-findAndAddNumbers = addNumbers . fromJust . decode . pack
+findAndAddNumbers = addNumbers . decodeJson
+
+findAndAddNonRedNumbers :: String -> Int
+findAndAddNonRedNumbers = addNonRedNumbers . decodeJson
+
+decodeJson :: String -> Value
+decodeJson = fromJust . decode . pack
 
 addNumbers :: Value -> Int
 addNumbers (Number n) = fromJust $ toBoundedInteger n
-addNumbers (Object o) = sum . map addNumbers $ HM.elems o
 addNumbers (Array a) = sum . map addNumbers $ V.toList a
+addNumbers (Object o) = sum . map addNumbers $ HM.elems o
 addNumbers _ = 0
+
+addNonRedNumbers :: Value -> Int
+addNonRedNumbers (Number n) = fromJust $ toBoundedInteger n
+addNonRedNumbers (Array a) = sum . map addNonRedNumbers $ V.toList a
+addNonRedNumbers (Object (HM.elems -> vs))
+  | any valueIsRed vs = 0
+  | otherwise = sum $ map addNonRedNumbers vs
+addNonRedNumbers _ = 0
+
+valueIsRed :: Value -> Bool
+valueIsRed (String t) | t == "red" = True
+valueIsRed _ = False
