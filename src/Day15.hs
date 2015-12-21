86e44ff3384ca245cc73e3681c90fa84528c9f3d
Patrick Herrmann <patrickwherrmann@gmail.com>
2015-12-21 11:37:27 -0800

    Finish day 15

--- src/Day15.hs
+++ src/Day15.hs
@@ -2,8 +2,30 @@ module Day15 where
 
 import Text.Parsec
 import Control.Monad
+import Data.List
 
-data Ingredient = Ingredient String [Int] Int deriving (Show)
+data Ingredient = Ingredient String [Int] deriving (Show)
+
+scores :: [Ingredient] -> [(Int, Int)]
+scores is = map score combos
+  where combos = map (`zip` is) (recipes $ length is)
+
+recipes :: Int -> [[Int]]
+recipes n = filter ((==100) . sum) $ replicateM n [0..100]
+
+score :: [(Int, Ingredient)] -> (Int, Int)
+score = nutrition . map scoreStat . transpose . map stats
+  where nutrition ss = (product $ init ss, last ss)
+
+stats :: (Int, Ingredient) -> [Int]
+stats (n, (Ingredient _ ps)) = map (*n) ps
+
+scoreStat :: [Int] -> Int
+scoreStat = atLeastZero . sum
+  where
+    atLeastZero x
+      | x < 0 = 0
+      | otherwise = x
 
 parseIngredients :: String -> [Ingredient]
 parseIngredients s = is
@@ -22,7 +44,7 @@ parseIngredients s = is
       texture <- num
       void $ string ", calories "
       cals <- num
-      return $ Ingredient n [cap, dur, flav, texture] cals
+      return $ Ingredient n [cap, dur, flav, texture, cals]
     name = many1 letter
     num = do
       sign <- optionMaybe $ char '-'
