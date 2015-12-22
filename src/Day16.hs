f4e0a8d4e9a80586bd933670ec0929e8cc8776d1
Patrick Herrmann <patrickwherrmann@gmail.com>
2015-12-22 09:45:04 -0800

    Finish day 16

--- src/Day16.hs
+++ src/Day16.hs
@@ -2,9 +2,39 @@ module Day16 where
 
 import Text.Parsec
 import Control.Monad
+import Data.Maybe
 
 type Compound = (String, Int)
-data Sue = Sue Int [Compound]
+type CompoundReading = String -> (Int -> Int -> Bool)
+data Sue = Sue Int [Compound] deriving (Show)
+
+isCorrectSue :: CompoundReading -> Sue -> Bool
+isCorrectSue r (Sue _ cs) = all compoundMatches detectedCompounds
+  where compoundMatches (k, v) = maybe True (flip (r k) v) (lookup k cs)
+
+detectedCompounds :: [Compound]
+detectedCompounds =
+  [ ("children", 3)
+  , ("cats", 7)
+  , ("samoyeds", 2)
+  , ("pomeranians", 3)
+  , ("akitas", 0)
+  , ("vizslas", 0)
+  , ("goldfish", 5)
+  , ("trees", 3)
+  , ("cars", 2)
+  , ("perfumes", 1)
+  ]
+
+trueCompoundReading :: CompoundReading
+trueCompoundReading "cats" = (>)
+trueCompoundReading "trees" = (>)
+trueCompoundReading "pomeranians" = (<)
+trueCompoundReading "goldfish" = (<)
+trueCompoundReading _ = (==)
+
+naiveCompoundReading :: CompoundReading
+naiveCompoundReading _ = (==)
 
 parseSues :: String -> [Sue]
 parseSues s = ss
