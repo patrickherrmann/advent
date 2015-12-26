93c73e172a0fde7dffbcdb641480dd7031691459
Patrick Herrmann <patrickwherrmann@gmail.com>
2015-12-25 20:21:56 -0800

    Solve part 2 of day 20

--- src/Day20.hs
+++ src/Day20.hs
@@ -6,9 +6,20 @@ guessSmallestFactor :: Int -> Int
 guessSmallestFactor n = i
   where (Just i) = find ((>=n).presents) [320000, 320320..]
 
+guessSmallestFactor2 :: Int -> Int
+guessSmallestFactor2 n = i
+  where (Just i) = find ((>=n).presents2) [320000, 320320..]
+
 factors :: Int -> [Int]
-factors n =1 : n : [x | x <- [2..n2], n `rem` x == 0]
+factors n = 1 : n : [x | x <- [2..n2], n `rem` x == 0]
   where n2 = n `div` 2 + n `rem` 2
+
+factors2 :: Int -> [Int]
+factors2 n = n : [x | x <- [1..n2], n `rem` x == 0, x * 50 >= n]
+  where n2 = n `div` 2 + n `rem` 2
+
+presents2 :: Int -> Int
+presents2 = sum . map (*11) . factors2
 
 presents :: Int -> Int
 presents = sum . map (*10) . factors
