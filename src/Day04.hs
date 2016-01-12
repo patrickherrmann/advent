663f80f42fd15cf43b4f0eff34f19d69f2d9f060
Patrick Herrmann <patrickwherrmann@gmail.com>
2016-01-11 17:54:15 -0800

    Use bytestring for md5 hash

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,14 +1,18 @@
 module Day04 where
 
-import Data.List
-import Data.Hash.MD5
+import Data.List (find)
+import Data.ByteString.Char8 (pack)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Crypto.Hash.MD5
+import Data.ByteString.Base16
 
 findAdventCoin :: String -> Int -> Int
 findAdventCoin key zs = i
-  where (Just i) = find (makesAdventCoin key zs) [1..]
-
-makesAdventCoin :: String -> Int -> Int -> Bool
-makesAdventCoin key zs i = zeros `isPrefixOf` hash
   where
-    hash = md5s (Str $ key ++ show i)
-    zeros = replicate zs '0'
+    (Just i) = find (makesAdventCoin (pack key) prefix) [1..]
+    prefix = BS.replicate zs 48 -- '0'
+
+makesAdventCoin :: ByteString -> ByteString -> Int -> Bool
+makesAdventCoin key prefix i = prefix `BS.isPrefixOf` encode coin
+  where coin = hash $ key `BS.append` (pack $ show i)
