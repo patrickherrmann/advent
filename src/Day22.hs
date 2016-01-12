1e51f540947818511e66e2140cfc122436ec468c
Patrick Herrmann <patrickwherrmann@gmail.com>
2016-01-11 17:47:26 -0800

    Begin implementing day 22

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,1 +1,20 @@
 module Day22 where
+
+type HitPoints = Int
+type Mana = Int
+
+data Wizard = Wizard
+  { hitPoints :: HitPoints
+  , mana :: Mana
+  , effects :: [Effect]
+  }
+
+data Effect = Effect
+  { duration :: Int
+  , effectType :: EffectType
+  }
+
+data EffectType
+  = Shield
+  | Poison
+  | Recharge
