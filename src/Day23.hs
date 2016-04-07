0589ba570e8b90470982d92acd61b4ace378e83b
Patrick Herrmann <patrickwherrmann@gmail.com>
2016-04-07 00:28:02 -0700

    Finish day 23 part 1

--- src/Day23.hs
+++ src/Day23.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE TemplateHaskell, RankNTypes #-}
+
 module Day23 where
 
+import Control.Lens
+import Control.Monad.State.Strict
 import Text.Parsec hiding (State)
+import qualified Data.Vector as V
 
 type Offset = Int
+type Program = V.Vector Instruction
 
 data Instruction
   = Hlf Register
@@ -18,8 +24,58 @@ data Register
   | B
   deriving (Show, Eq)
 
-parseInstructions :: String -> [Instruction]
-parseInstructions s = is
+data Machine = Machine
+  { _ra :: Int
+  , _rb :: Int
+  , _pc :: Int
+  }
+  deriving (Show)
+
+makeLenses ''Machine
+
+executeProgram :: Program -> Machine
+executeProgram p = execState (runProgram p) initialMachine
+
+runProgram :: Program -> State Machine ()
+runProgram p = do
+  pci <- use pc
+  case p V.!? pci of
+    Nothing -> return ()
+    Just i -> do
+      runInstruction pci i
+      runProgram p
+
+runInstruction :: Int -> Instruction -> State Machine ()
+runInstruction i = \case
+  Hlf r -> reg r %= (`div` 2) >> pc += 1
+  Tpl r -> reg r *= 3 >> pc += 1
+  Inc r -> reg r += 1 >> pc += 1
+  Jmp o -> pc .= i + o
+  Jie r o -> do
+    rv <- use (reg r)
+    if even rv
+      then pc .= i + o
+      else pc += 1
+  Jio r o -> do
+    rv <- use (reg r)
+    if rv == 1
+      then pc .= i + o
+      else pc += 1
+
+reg :: Register -> Lens' Machine Int
+reg = \case
+  A -> ra
+  B -> rb
+
+initialMachine :: Machine
+initialMachine = Machine
+  { _ra = 0
+  , _rb = 0
+  , _pc = 0
+  }
+
+parseProgram :: String -> Program
+parseProgram s = V.fromList is
   where
     Right is = parse instructions "" s
     instructions = instruction `sepBy` endOfLine
